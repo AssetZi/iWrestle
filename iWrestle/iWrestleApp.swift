d3489a658de8cd34227033750b4fd18c534e057e
AssetZi <109986632+AssetZi@users.noreply.github.com>
2025-11-25 13:24:18 -0500

    Initial Commit

---
+++ iWrestle/iWrestleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWrestleApp.swift
+//  iWrestle
+//
+//  Created by Brock Zacherl on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct iWrestleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
